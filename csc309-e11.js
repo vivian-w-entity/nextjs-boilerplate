04856fd54c1398c83732f24d845a9db1ff36df2a
Vivian White <vivian.white@mail.utoronto.ca>
2024-11-27 23:25:26 -0500

    Submit E11

---
+++ csc309-e11.js
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <div id="auto-tester">
+            Hi. I am 1009104979, and I have learned how to develop a full-stack web app
+            and deploy it!
+        </div>
+    )
+}
